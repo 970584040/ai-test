bcfe4b8a5b3515c4a3e0f555598dfaeb013b9ecb
dzsusses <dzsusses@163.com>
2025-07-05 15:36:16 +0800

    温度和top-k

--- train/participle.py
+++ train/participle.py
@@ -21,6 +21,29 @@ class GPTDatasetV1(Dataset):
 
     def __getitem__(self, idx):
         return self.input_ids[idx], self.target_ids[idx]
+    
+def generate(model, idx, max_new_tokens, context_siez, temperature=0.0, top_k=None, eos_id=None):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:,-context_siez:]
+        with torch.no_grad():
+            logits, _ = model(idx_cond)
+        logits = logits[:, -1, :]
+        if top_k is not None:
+            top_logits, top_indices = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(logits < min_val, torch.tensor(float('-inf')).to(logits.device), logits)
+        probs = torch.softmax(logits, dim=-1)
+        if temperature > 0:
+            logits = logits / temperature
+            probs = torch.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+        if idx_next == eos_id:
+            break
+        idx = torch.cat((idx, idx_next), dim=1)
+    return idx
+            
 
 def create_dataloader_v1(txt, batch_size=4, max_length=512, stride=128, shuffle=True, drop_last=True, num_workers=0):
     tokenizer = tiktoken.get_encoding("gpt2")
